405b269897deb3a6a56b0379667ed29292378da6
Ylli Petrovci <yllipetrovci@Yllis-MacBook-Pro.local>
2020-07-12 01:34:25 +0200

    Update products mock data for

--- src/pages/home-screen.js
+++ src/pages/home-screen.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import MenuHeader from '../components/menu-header';
-import ListProducts from '../components/list-product';
+import ListProducts from '../components/list-products';
 
 const HomeScreen = () => {
     const weeklyProductsData = [
-        { price: 49.99, title: 'Women Shoes', img: '' },
-        { price: 14.99, title: 'Peony Bouqet', img: '' },
-        { price: 23.99, title: 'Blank Books', img: '' },
-        { price: 3.99, title: 'Tennis Ball', img: '' },
-        { price: 1.99, title: 'Pencils', img: '' },
-        { price: 99.99, title: 'Converse', img: '' },
+        { id:1,price: 49.99, title: 'Women Shoes', img: '' },
+        { id:2,price: 14.99, title: 'Peony Bouqet', img: '' },
+        { id:3,price: 23.99, title: 'Blank Books', img: '' },
+        { id:4,price: 3.99, title: 'Tennis Ball', img: '' },
+        { id:5,price: 1.99, title: 'Pencils', img: '' },
+        { id:6,price: 99.99, title: 'Converse', img: '' },
     ];
 
     const mostViewdData = [
-        { price: 3.99, title: 'Tennis Ball', img: '' },
-        { price: 1.99, title: 'Pencils', img: '' },
-        { price: 99.99, title: 'Converse', img: '' },
+        { id:1,price: 3.99, title: 'Tennis Ball', img: '' },
+        { id:2,price: 1.99, title: 'Pencils', img: '' },
+        { id:3,price: 99.99, title: 'Converse', img: '' },
     ]
 
     return (
