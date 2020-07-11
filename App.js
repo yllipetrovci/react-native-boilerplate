63eaa8c80dac0533d6ca3d343ded4742b472bf04
Ylli Petrovci <yllipetrovci@Yllis-MacBook-Pro.local>
2020-07-12 00:54:57 +0200

    Import home screen

--- App.js
+++ App.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+/**
+ * Screens
+ */
+import HomeScreen from './src/pages/home-screen';
 
 const Stack = createStackNavigator();
+const defaultOptions = {
+  headerShown: false
+};
 
 const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        {/* <Stack.Screen
+        <Stack.Screen
           name="Home"
           component={HomeScreen}
-          options={{ title: 'Welcome' }}
-        /> */}
+          options={defaultOptions}
+        />
         {/* <Stack.Screen name="Profile" component={ProfileScreen} /> */}
       </Stack.Navigator>
     </NavigationContainer>
