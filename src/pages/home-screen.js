b15217e56a3af013df6848a09f921dd75a8119c4
Ylli Petrovci <yllipetrovci@Yllis-MacBook-Pro.local>
2020-07-12 01:30:25 +0200

    Send mock data for showing the product

--- src/pages/home-screen.js
+++ src/pages/home-screen.js
@@ -4,11 +4,26 @@ import MenuHeader from '../components/menu-header';
 import ListProducts from '../components/list-product';
 
 const HomeScreen = () => {
+    const weeklyProductsData = [
+        { price: 49.99, title: 'Women Shoes', img: '' },
+        { price: 14.99, title: 'Peony Bouqet', img: '' },
+        { price: 23.99, title: 'Blank Books', img: '' },
+        { price: 3.99, title: 'Tennis Ball', img: '' },
+        { price: 1.99, title: 'Pencils', img: '' },
+        { price: 99.99, title: 'Converse', img: '' },
+    ];
+
+    const mostViewdData = [
+        { price: 3.99, title: 'Tennis Ball', img: '' },
+        { price: 1.99, title: 'Pencils', img: '' },
+        { price: 99.99, title: 'Converse', img: '' },
+    ]
+
     return (
         <View>
             <MenuHeader />
-            <ListProducts title='Weekly Products'/>
-            <ListProducts title='Most Viewed'/>
+            <ListProducts title='Weekly Products' data={weeklyProductsData} />
+            <ListProducts title='Most Viewed' data={mostViewdData} />
         </View>
     );
 };
