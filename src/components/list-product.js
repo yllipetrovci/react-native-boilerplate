2d4181bbef2d040a888fdb10f24f3fd40ff38d61
Ylli Petrovci <yllipetrovci@Yllis-MacBook-Pro.local>
2020-07-12 01:23:42 +0200

    Implement main sceleton for list product component

--- src/components/list-product.js
+++ src/components/list-product.js
@@ -1,10 +1,30 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 
-const ListProducts = () => {
+const ListProducts = ({ title }) => {
     return (
         <View style={styles.container}>
-            <Text>List Products</Text>
+            <View style={styles.header}>
+                <Text style={styles.headerTitle}>{title}</Text>
+                <Text>View all</Text>
+            </View>
+            <View>
+                <FlatList
+                    horizontal={true}
+                    data={[1, 2, 3, 4]}
+                    showsHorizontalScrollIndicator={false}
+                    renderItem={({ item }) => (
+                        <View style={{
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            borderWidth:1,
+                        }}>
+                            <Text onPress={() => onPress(item.id)}>{item}</Text>
+                        </View>
+                    )}
+                    keyExtractor={(item) => item.toString()}
+                />
+            </View>
         </View>
     );
 };
@@ -14,5 +34,12 @@ export default ListProducts;
 const styles = StyleSheet.create({
     container: {
         borderWidth: 1
+    },
+    header: {
+        borderWidth: 1,
+        borderColor: 'red'
+    },
+    headerTitle: {
+        color: 'green'
     }
 });
--- src/pages/home-screen.js
+++ src/pages/home-screen.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import MenuHeader from '../components/menu-header';
+import ListProducts from '../components/list-product';
 
 const HomeScreen = () => {
     return (
         <View>
             <MenuHeader />
-            <Text>HomeScreen</Text>
+            <ListProducts title='Weekly Products'/>
+            <ListProducts title='Most Viewed'/>
         </View>
     );
 };
