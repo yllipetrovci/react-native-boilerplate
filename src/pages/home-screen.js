9dfa49657dec3b7e33675b98bde1ef71da6a1ff5
Ylli Petrovci <yllipetrovci@Yllis-MacBook-Pro.local>
2020-07-12 01:51:46 +0200

    Change background color for  home screen, format code

--- src/pages/home-screen.js
+++ src/pages/home-screen.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import MenuHeader from '../components/menu-header';
 import ListProducts from '../components/list-products';
 
 const HomeScreen = () => {
     const weeklyProductsData = [
-        { id:1,price: 49.99, title: 'Women Shoes', img: '' },
-        { id:2,price: 14.99, title: 'Peony Bouqet', img: '' },
-        { id:3,price: 23.99, title: 'Blank Books', img: '' },
-        { id:4,price: 3.99, title: 'Tennis Ball', img: '' },
-        { id:5,price: 1.99, title: 'Pencils', img: '' },
-        { id:6,price: 99.99, title: 'Converse', img: '' },
+        { id: 1, price: 49.99, title: 'Women Shoes', img: '' },
+        { id: 2, price: 14.99, title: 'Peony Bouqet', img: '' },
+        { id: 3, price: 23.99, title: 'Blank Books', img: '' },
+        { id: 4, price: 3.99, title: 'Tennis Ball', img: '' },
+        { id: 5, price: 1.99, title: 'Pencils', img: '' },
+        { id: 6, price: 99.99, title: 'Converse', img: '' },
     ];
 
     const mostViewdData = [
-        { id:1,price: 3.99, title: 'Tennis Ball', img: '' },
-        { id:2,price: 1.99, title: 'Pencils', img: '' },
-        { id:3,price: 99.99, title: 'Converse', img: '' },
+        { id: 1, price: 3.99, title: 'Tennis Ball', img: '' },
+        { id: 2, price: 1.99, title: 'Pencils', img: '' },
+        { id: 3, price: 99.99, title: 'Converse', img: '' },
     ]
 
     return (
-        <View>
+        <View style={styles.container}>
             <MenuHeader />
             <ListProducts title='Weekly Products' data={weeklyProductsData} />
             <ListProducts title='Most Viewed' data={mostViewdData} />
@@ -29,3 +29,10 @@ const HomeScreen = () => {
 };
 
 export default HomeScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#fff',
+        height:'100%'
+    }
+})
