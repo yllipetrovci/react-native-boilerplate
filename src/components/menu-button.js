8530cfba4d7e4897a5d48a5b15a76ff901243b24
Ylli Petrovci <yllipetrovci@Yllis-MacBook-Pro.local>
2020-07-12 02:15:17 +0200

    Implement structure style for menu button and header

--- src/components/menu-button.js
+++ src/components/menu-button.js
@@ -3,11 +3,14 @@ import { View, Text, StyleSheet } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const MenuButton = ({ item, onPress }) => {
-    const { title, price, img } = item;
-    
+    const { icon, name, bgColor } = item;
+
     return (
-        <TouchableOpacity onPress={onPress} style={styles.container}>
-         <Text>Menu button</Text>
+        <TouchableOpacity onPress={onPress} >
+            <View style={styles.container}>
+                <View style={[styles.imageVw, { backgroundColor: bgColor }]} />
+                <Text style={styles.nameTxt}>{name}</Text>
+            </View>
         </TouchableOpacity>
     );
 };
@@ -17,5 +20,17 @@ export default MenuButton;
 const styles = StyleSheet.create({
     container: {
         borderWidth: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100%',
+        width: 102
     },
+    imageVw: {
+        width: 65,
+        height: 65,
+        borderRadius: 50
+    },
+    nameTxt:{
+        marginVertical:5
+    }
 });
--- src/components/menu-header.js
+++ src/components/menu-header.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+import MenuButton from './menu-button';
 
-const MenuHeader = () => {
+const MenuHeader = ({ title, data }) => {
+
+    const onPress = (id) => {
+        console.log({ id });
+    }
+
     return (
         <View style={styles.container}>
-            <Text>Menu Header</Text>
+            <View style={styles.headerVw}>
+                <Text style={styles.headerTitleTxt}>{title}</Text>
+            </View>
+            <View style={styles.listProductsVw}>
+                <FlatList
+                    horizontal={true}
+                    data={data}
+                    showsHorizontalScrollIndicator={false}
+                    renderItem={({ item }) => (
+                        <MenuButton item={item} onPress={() => { onPress(item.id) }} />
+                    )}
+                    keyExtractor={(item) => item.name.toString()}
+                />
+            </View>
         </View>
     );
 };
@@ -14,5 +33,23 @@ export default MenuHeader;
 const styles = StyleSheet.create({
     container: {
         borderWidth: 1
+    },
+    headerVw: {
+        borderWidth: 1,
+        borderColor: 'red',
+        flexDirection:'row',
+        justifyContent:'flex-start',
+        marginHorizontal:10
+    },
+    headerTitleTxt: {
+        color: 'green'
+    },
+    listProductsVw: {
+        borderWidth:1,
+        height:100,
+        flexDirection:'row',
+        marginHorizontal:10
+        // flexDirection:'column',
+        // justifyContent:'center',
     }
 });
