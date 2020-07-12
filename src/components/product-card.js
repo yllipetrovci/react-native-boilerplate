dbf6715ebc423d24d59a6b23bbe460ef85c5c23b
Ylli Petrovci <yllipetrovci@Yllis-MacBook-Pro.local>
2020-07-12 02:25:57 +0200

    Implement colors for product card

--- src/components/product-card.js
+++ src/components/product-card.js
@@ -4,12 +4,12 @@ import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const ProductCard = ({ item, onPress }) => {
     const { title, price, img } = item;
-    
+
     return (
         <TouchableOpacity onPress={onPress} style={styles.container}>
             <View style={styles.imageVw}><Text>IMG</Text></View>
             <Text style={styles.titleTxt}>{title}</Text>
-            <Text style={styles.priceTxt}>{price}</Text>
+            <Text style={styles.priceTxt}>$ {price}</Text>
         </TouchableOpacity>
     );
 };
@@ -19,16 +19,25 @@ export default ProductCard;
 const styles = StyleSheet.create({
     container: {
         borderWidth: 1,
-        width:150,
-        height:200,
-        margin:10
+        borderColor: '#CAD8E8',
+        borderRadius: 10,
+        width: 150,
+        height: 200,
+        margin: 10,
+        backgroundColor: '#fff'
     },
     imageVw: {
-        borderWidth:1,
-        borderColor:'red',
-        height:'60%'
+        borderBottomWidth: 1,
+        borderColor: '#F7F7F7',
+        height: '60%'
     },
     titleTxt: {
-
+        fontSize: 13,
+        color: '#515C6F'
+    },
+    priceTxt: {
+        fontSize: 12,
+        color: '#6E6B90',
+        fontWeight: 'bold'
     }
 });
